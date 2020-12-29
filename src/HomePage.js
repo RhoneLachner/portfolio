c9505065cdfaec03a7642e729a661ec9b03f49a1
Rhone Lachner <fernandclay@gmail.com>
2020-12-28 21:26:35 -0800

    added construction home message

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,7 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className="container">
-            <h1 className="glow">Hello! Welcome to the home page. This website is currently under construction.</h1>
+            <h1 className="glow">Hello! Welcome to the home page. <br/>This website is currently under construction.</h1>
         </div>
         )
     }
