bac5b15abff5b279a89a292cd8aec763da4a0b1d
Rhone Lachner <fernandclay@gmail.com>
2020-12-28 20:42:26 -0800

    created file structure and home page appears on dev screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+
+import ContactPage from './ContactPage.js';
+import LoadingPage from './LoadingPage.js';
+import MusicPage from './MusicPage.js';
+import HomePage from './HomePage.js';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends React.Component {
+  render() {
+      return (
+
+          <div>
+              <Router>               
+                  <Switch>
+                      <Route 
+                          path="/" 
+                          exact
+                          render={(routerProps) => <HomePage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/loading" 
+                          exact
+                          render={(routerProps) => <LoadingPage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/music" 
+                          exact
+                          render={(routerProps) => <MusicPage {...routerProps} />} 
+                      />
+                      <Route 
+                          path="/contact" 
+                          exact
+                          render={(routerProps) => <ContactPage {...routerProps} />} 
+                      />
+                  </Switch>
+              </Router>
+          </div>
+
+      )
+  }
 }
 
-export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div className="container">
+            <h1 className="glow">Hello! Welcome to the home page.</h1>
+        </div>
+        )
+    }
+}
