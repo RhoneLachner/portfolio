d80df8a7c809b4cbc65b32ef84e9771d89fb6a48
Rhone Lachner <fernandclay@gmail.com>
2021-02-23 15:28:15 -0800

    fixed typo

--- src/Bio.js
+++ src/Bio.js
@@ -10,7 +10,7 @@ export default class Bio extends Component {
                 <div className='bioDiv'>
                     <FadeInAnimation wrapperElement="p"   className='bioText'>
                         With a creative eye, positive outlook, adaptive resilience, and broad skillset, I bring a
-                        methodical and wholistic approach to all that I do. <br/><br/>
+                        methodical and holistic approach to all that I do. <br/><br/>
                         I am an innovative problem solver, empathetic team builder, and communicative collaborator thriving in both team and solo environments, <br/>
                         and I believe that every business is made better with an inclusive and diverse array of perspectives and backgrounds. <br/><br/>
                         in 2021 I aim to create memorable, useful, and aesthetically pleasing applications that enrich people's lives,
