86b2458a13c61e3b8981c7bb04fe732e6a154c9e
Rhone Lachner <fernandclay@gmail.com>
2021-02-25 00:46:52 -0800

    updated circle font

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,7 +8,7 @@ import StandFor from './StandFor.js';
 import MiscSkills from './MiscSkills';
 import SayHello from './SayHello.js';
 
-import circleText from './images/circle-text.png';
+import circleText from './images/circle-text3.png';
 import rhoneFluteDark from './images/rhone-dark-flute2.jpg';
 import downArrow from './images/down-arrow-pretty.png';
 
