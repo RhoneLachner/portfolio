22c438d59ebdf28748e4dcd4466e6226b375cc7b
Rhone Lachner <fernandclay@gmail.com>
2022-01-13 18:54:14 -0800

    bio text formatting update

--- src/Bio.js
+++ src/Bio.js
@@ -11,9 +11,10 @@ export default class Bio extends Component {
                         With a creative eye, positive outlook, adaptive resilience, and broad skillset, I bring a
                         methodical and holistic approach to all that I do. <br/><br/>
                         I am an innovative problem solver, empathetic team builder, and communicative collaborator thriving in both team and solo environments, <br/>
-                        and I believe that every business is made better with an inclusive and diverse array of perspectives and backgrounds. <br/><br/>
+                        and I believe that every business is made better with an inclusive and diverse array of perspectives and backgrounds.
+                        <br/><br/>
                         In 2022 I am focusing much of my energy on the creative aspects of coding, accessibility, and learning new mediums 
-                        to help bridge the gap <br></br> between physical and digital worlds. 
+                        to help bridge the gap between physical and digital worlds. 
                     </FadeInAnimation>
                 </div>
             </>
