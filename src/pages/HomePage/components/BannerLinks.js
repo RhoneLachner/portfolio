741c015706c0d40583bef98519a05934b11fbf55
Rhone Lachner <fernandclay@gmail.com>
2023-05-09 00:57:35 -0700

    updated music link to hopeandfailure site

--- src/pages/HomePage/components/BannerLinks.js
+++ src/pages/HomePage/components/BannerLinks.js
@@ -1,23 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { ExternalLink } from "react-external-link";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-export default class BannerLinks extends Component {
-  render() {
-    return (
-      <>
-        <div className="externalLinks">
-          <ExternalLink href="https://github.com/RhoneLachner">
-            <span className="code">Coding</span>
-          </ExternalLink>
-          <ExternalLink href="https://www.linkedin.com/in/rhonelachner/">
-            <span className="linkedIn">LinkedIn</span>
-          </ExternalLink>
-          <Link className="music" to="/music">
+const BannerLinks = () => {
+  return (
+    <>
+      <div className="externalLinks">
+        <ExternalLink href="https://github.com/RhoneLachner">
+          <span className="code">Coding</span>
+        </ExternalLink>
+        <ExternalLink href="https://www.linkedin.com/in/rhonelachner/">
+          <span className="linkedIn">LinkedIn</span>
+        </ExternalLink>
+        <ExternalLink href="https://hopeandfailure.com">
+          <span className="music">Music</span>
+        </ExternalLink>
+
+        {/* <Link className="music" to="/music">
             Music
-          </Link>
-        </div>
-      </>
-    );
-  }
-}
+          </Link> */}
+      </div>
+    </>
+  );
+};
+
+export default BannerLinks;
